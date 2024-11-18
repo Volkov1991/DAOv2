d1feafe322db21459698b7f8ceab1db4f186050e
Volkov Vadim <vad.volkov91@gmail.com>
2024-11-19 03:32:35 +0700

    Initial commit

---
+++ src/main/java/netology/daov2/DaOv2Application.java
@@ -0,0 +1,13 @@
+package netology.daov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaOv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaOv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/netology/daov2/DaOv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package netology.daov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaOv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
